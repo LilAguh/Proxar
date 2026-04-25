25df0d251f3159ebcd396e433dfb95ffc464dcd8
LilAguh <aguezeochoa@gmail.com>
2026-04-25 20:03:07 -0300

    update: sync client entity with backend model

--- src/core/entities/Client.entity.ts
+++ src/core/entities/Client.entity.ts
@@ -1,10 +1,12 @@
 export interface Client {
   id: string;
+  companyId: string;
   name: string;
   phone: string;
   email?: string;
   address?: string;
   notes?: string;
+  active: boolean;
   createdAt: string;
   modifiedAt: string;
 }
