cbd11091e27ca77ff679214409028074625d62f2
LilAguh <aguezeochoa@gmail.com>
2026-04-25 20:03:07 -0300

    update: sync box movement entity with backend model

--- src/core/entities/BoxMovement.entity.ts
+++ src/core/entities/BoxMovement.entity.ts
@@ -1,12 +1,17 @@
-import { MovementType, PaymentMethod } from '../enums';
+import { MovementType, PaymentMethod } from '@core/enums';
 import { Account } from './Account.entity';
+import { Ticket } from './Ticket.entity';
 import { User } from './User.entity';
 
 export interface BoxMovement {
   id: string;
+  companyId: string;
   number: number;
+  accountId: string;
   account: Account;
-  ticketNumber?: number;
+  ticketId?: string;
+  ticket?: Ticket;
+  userId: string;
   user: User;
   type: MovementType;
   amount: number;
@@ -14,6 +19,7 @@ export interface BoxMovement {
   concept: string;
   voucherNumber?: string;
   observations?: string;
+  active: boolean;
   movementDate: string;
   registeredAt: string;
 }
