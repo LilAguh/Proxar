ba645d4b873b76e9e3e74b2c454597064d1a6c10
LilAguh <aguezeochoa@gmail.com>
2026-04-25 20:03:08 -0300

    update: sync user entity with backend model

--- src/core/entities/User.entity.ts
+++ src/core/entities/User.entity.ts
@@ -1,9 +1,12 @@
-import { UserRole } from '../enums';
+import { UserRole } from '@core/enums';
 
 export interface User {
   id: string;
+  companyId: string;
   name: string;
   email: string;
   role: UserRole;
   active: boolean;
+  createdAt?: string;
+  modifiedAt?: string;
 }
