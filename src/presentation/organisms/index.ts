deb081f4b33815e523888093fca3c93674176920
LilAguh <aguezeochoa@gmail.com>
2026-04-30 19:34:37 -0300

    frontend: exporta auth card

--- src/presentation/organisms/index.ts
+++ src/presentation/organisms/index.ts
@@ -1,3 +1,4 @@
 export { Sidebar } from './Sidebar/Sidebar';
 export { Topbar } from './Topbar/Topbar';
 export { MainLayout } from './MainLayout/MainLayout';
+export { AuthCard } from './AuthCard/AuthCard';
