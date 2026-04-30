770b2d38946b81e8087c524ba5f2271893429c94
LilAguh <aguezeochoa@gmail.com>
2026-04-30 19:34:37 -0300

    frontend: integra modales en layout

--- src/presentation/organisms/MainLayout/MainLayout.tsx
+++ src/presentation/organisms/MainLayout/MainLayout.tsx
@@ -1,9 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import { Sidebar } from '../Sidebar/Sidebar';
 import { Topbar } from '../Topbar/Topbar';
+import { ToastContainer } from '@presentation/molecules';
+import { ModalNuevoTicket, ModalCaja, ModalClient } from '@presentation/features';
+import { useUIStore } from '@/stores';
 import './MainLayout.scss';
 
 export const MainLayout = () => {
+  const { modalTicket, closeModalTicket, modalCaja, closeModalCaja, modalClient, closeModalClient } = useUIStore();
+
   return (
     <div className="main-layout">
       <Sidebar />
@@ -13,6 +18,11 @@ export const MainLayout = () => {
           <Outlet />
         </div>
       </main>
+
+      <ModalNuevoTicket isOpen={modalTicket} onClose={closeModalTicket} />
+      <ModalCaja isOpen={modalCaja} onClose={closeModalCaja} />
+      <ModalClient isOpen={modalClient} onClose={closeModalClient} />
+      <ToastContainer />
     </div>
   );
 };
